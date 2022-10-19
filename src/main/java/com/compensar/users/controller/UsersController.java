fc9398cf1e5f24725be69c5fb29bf022b9627f09
KevinJimenez <kevin.jimenez@credibanco.com>
2022-10-18 22:04:44 -0500

    Create service json

--- src/main/java/com/compensar/users/controller/UsersController.java
+++ src/main/java/com/compensar/users/controller/UsersController.java
@@ -2,18 +2,27 @@ package com.compensar.users.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.compensar.users.service.ExcelService;
+
 @RestController
-@RequestMapping("users")
+@RequestMapping("publico/users")
 public class UsersController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(UsersController.class);
 
+
+	@Autowired
+	private ExcelService serviceExcel;
+	
 	@RequestMapping(method = RequestMethod.GET, value = "/hello")
 	public String helloWorld() {
 		
@@ -22,6 +31,12 @@ public class UsersController {
 		logger.info("{}", auth.toString());
 		
 		return "Hello World";
+	}
+	
+	@GetMapping("/info")
+	public String info() {
+		
+		return serviceExcel.infoReturn();
 	}
 	
 }
--- src/main/java/com/compensar/users/service/ExcelService.java
+++ src/main/java/com/compensar/users/service/ExcelService.java
@@ -10,4 +10,6 @@ public interface ExcelService {
 
 	List<ConsolidadoData> extractDataExcel(MultipartFile file);
 	
+	String infoReturn();
+	
 }
